f7b3695693461a18e72d98f0ca8e7815e2643293
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-03-25 11:45:47 +0530

    Sorted the product prices using quick sort.

---
+++ QuickSort.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        double[] productPrices = {899.99, 299.50, 499.75, 150.25, 650.00, 399.99};
+        quickSort(productPrices, 0, productPrices.length - 1);
+        System.out.println("Sorted Product Prices: " + Arrays.toString(productPrices));
+    }
+
+    public static void quickSort(double[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    public static int partition(double[] arr, int low, int high) {
+        double pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                double temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        double temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+}
